ffef62d3826e0395727bf5a12630198bbfcc8db0
allenkong221 <allen.kong@uconn.edu>
2022-08-27 12:01:10 -0400

    updated /api/:date?

--- index.js
+++ index.js
@@ -24,7 +24,24 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", function (req, res) {
+  var date = req.params.date
 
+  if(!date){
+    let dateObj = new Date()
+    res.json({'unix':dateObj.getTime(), 'utc':dateObj.toUTCString()})
+  }else{
+    let dateObj = new Date(date)
+    if (dateObj.toString() == 'Invalid Date'){
+      dateObj = new Date(parseInt(req.params.date));
+    }
+    if (dateObj.toUTCString() == 'Invalid Date'){
+      res.json({error:"Invalid Date"})
+    }else{
+      res.json({'unix':dateObj.getTime(), 'utc':dateObj.toUTCString()})
+    }
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
